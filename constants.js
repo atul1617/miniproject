bfdf4ae711b6e0464947dbd61f8f44604a3ef607
ATUL kumar <57282448+atul1617@users.noreply.github.com>
2021-12-29 21:46:31 +0530

    output
    https://success-academy-41f44.web.app/

---
+++ constants.js
@@ -0,0 +1,44 @@
+// Options the user could type in
+const prompts = [
+  ["hi", "hey", "hello", "HELLO", "HEY", "hii", "HI"],
+  ["about success", "tell about institute", "about your institute", "TELL ABOUT SUCCESS", "ABOUT INSTITUTE", "SUCCESS", "tell me about institute", "TELL ME ABOUT SUCCESS"],
+  ["course"],
+  ["programming language"],
+  ["what is this institute bond rating"],
+  ["facility", "FACILITY"],
+  ["fees structure", "fees", "FEES", "FEES STRUCTURE", "fees structure of buit", "tell me fees structure of buit"],
+  ["Gate", "gate", "GATE"],
+  ["c", "C", "c++", "C++", "java", "JAVA", "python", "PYTHON"],
+  ["what", "why", "how", "where", "when"],
+  [""],
+  ["haha", "ha", "hehe", "funny", "joke"],
+  ["HELP ME", "help me"],
+  ["vision", "VISION", "vision of buit", "VISION OF BUIT", "buit vision", "BUIT VISION"],
+  ["mission", "MISSION", "MISSION OF BUIT", "mission of buit", "buit mission", "BUIT MISSION"],
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+ ["u"],
+  ["Over the past 5 years, the team has developed the course curriculum and teaching methodology for the GATE examination. This has also taken a tangible shape in the development of preparatory programs for entrance and aptitude tests for various Public/Government Sector jobs. The design, development and delivery of each of the programs offered at The SUCCESS ACADEMY incorporates the best teaching practices relevant to the tests. "],
+  ["our institiute cureently provide only GATE classes. after some times i have launches some more new coureses."],
+  ["our institute also provide  programming language  batches like- c, c++, java, python, javascript."],
+  ["4.1"],
+  ["there are online library,virtual lab are there in the institute. faculty provide workshop internship. monthly mock test avialable in online."],
+  ["Which course do you want to know the fee structure of?"],
+  ["Total year wise fees 1 year course = 10000/- , 2 year course = 15000/-"],
+  ["6 month course fee = 3000/-, 1 year course fee + 5 project = 5000/-"],
+  ["Great question"],
+  ["Please say something :("],
+  ["Haha!", "Good one!"],
+  ["How can help you, tell me"],
+  ["HELLO, how can i help you", "HI, how can i help you", "HEY!, how can I help yoSS ACADEMY has been established with the goal of imparting technical education to the students, keeping in view the global technological developments and to meet the requirements of the Engineers and transform them into professionals of distinguishable standards.Initiating programmes & continously enhancing the quality of the students. Our aim is to create an entrepreneurial attitude, spirit and result-oriented motivation among budding engineers. Endeavour to become a recognized center of high level technological research and learning in the country."],
+  ["The mission of SUCCESS ACADEMY will be to provide every opportunity for each students to attain the best of their capabilities & create in them the desire to excel. The seeds for the craving for excellence would be sown in the minds of the students as soon as they enter the portals of SUCCESS ACADEMY. The SUCCESS ACADEMY act as a catalyst in creating excellence in technical education. Promoting convergence of knowledge, Information, Technology & Skills. It also provide the requisite know how and generate a work force with the right kind of professional, Instincts,Values,Attitude and expertise."]
+]
+
+
+const alternative = [
+  "I don't understand :/",
+  "sorry I don't understand :/"
+]
